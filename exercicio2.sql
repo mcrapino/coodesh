45f9087ca2dd24ffad693186cf2b6136be0031ad
mcrapino <mcrapino@yahoo.com>
2025-06-23 21:35:50 -0300

    Update exercicio2.sql

--- exercicio2.sql
+++ exercicio2.sql
@@ -1,1 +1,1 @@
-select product_id, product_name, brand_id, category_id, model_year, list_price from products where product_id not in (select product_id from order_items) ordef by product_name;
+select product_id, product_name, brand_id, category_id, model_year, list_price from products where product_id not in (select product_id from order_items) order by product_name;
