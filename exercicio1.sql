aeb76362d460333799c50a362618b4a920bcc8da
mcrapino <mcrapino@yahoo.com>
2025-06-23 21:35:29 -0300

    Update exercicio1.sql

--- exercicio1.sql
+++ exercicio1.sql
@@ -1,1 +1,1 @@
-select customer_id, fist_name, last_name, phone, email, street, city, state_zip_code from  customers where customers where customer_id not in (select customer_id from orders) order by fistname, last_name;
+select customer_id, fist_name, last_name, phone, email, street, city, state_zip_code from  customers where customer_id not in (select customer_id from orders) order by fistname, last_name;
